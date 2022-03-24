1310d8edf5d9c626f334b22fb2793ecd0a2daa29
Phat Vo <votanphatb2@gmail.com>
2022-03-24 14:44:51 +0700

    fix: remove to boot scene

--- src/hooks/useScene.js
+++ src/hooks/useScene.js
@@ -1,8 +1,8 @@
 import create from 'zustand';
-import { BATTE_SCENE } from 'constants/scene';
+import { BOOT_SCENE } from 'constants/scene';
 
 const useScene = create((set) => ({
-  scene: BATTE_SCENE,
+  scene: BOOT_SCENE,
   setScene: (scene) => set({ scene }),
 }));
 
